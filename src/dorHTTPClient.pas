707d66cbf822b03f25b5326ecc48b67ffffe3fa7
Pierre Yager <pierre.yager@crisalid.com>
2017-11-16 12:10:34 +0100

    If NO_SSLV3 is defined, set ssl context option to disable SSLv3

--- src/dorHTTPClient.pas
+++ src/dorHTTPClient.pas
@@ -917,6 +917,9 @@ begin
   if ssl then
   begin
     FCtx := SSL_CTX_new(SSLv23_method);
+  {$IFDEF NO_SSLV3}
+    SSL_CTX_set_options(FCtx, SSL_OP_NO_SSLv3);
+  {$ENDIF}
     SSL_CTX_set_cipher_list(FCtx, 'DEFAULT');
 
     SSL_CTX_set_default_passwd_cb_userdata(FCtx, Self);
